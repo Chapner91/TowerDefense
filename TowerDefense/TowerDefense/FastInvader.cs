73697cfc3d9c7a5f051e38120a541e38ef8d78d4
Charles Hapner <chapner91@gmail.com>
2019-04-25 17:41:14 -0400

    Added fast invader class

---
+++ TowerDefense/TowerDefense/FastInvader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace TowerDefense
+{
+	class FastInvader : Invader
+	{
+		private int _pathStep = 0;
+
+		public FastInvader(Path path) : base(path)
+		{
+
+		}
+
+
+		public void Move()
+		{
+			_pathStep += 2;
+		}
+	}
+}
